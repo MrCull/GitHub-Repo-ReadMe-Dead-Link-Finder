46df5294c161f8a143b25bba9ddaf716a99d1010
Mark Cullen <mark.cullen@tba.group>
2020-11-01 01:49:13 +0000

    first pass a mvp web front end

---
+++ DeadLinkFinderWeb/Models/RepoChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeadLinkFinderWeb.Models
+{
+    public class RepoChecker
+    {
+        [Range(0, 25, ErrorMessage = "Enter number between 0 and 25")]
+        [Display(Name = "Maximum number of Repos")]
+        public int NumberOfReposToSearchFor { get; set; }
+
+        [Display(Name = "Single Repo check URI")]
+        public string SingleRepoUri { get; set; }
+
+        [Display(Name = "Minimum Repo Star rating")]
+        public int? MinStar { get; set; }
+
+        [Display(Name = "Repos update after")]
+        public DateTime? UpdatedAfter { get; set; }
+
+        [Display(Name = "Repos belonging to User")]
+        public string User { get; set; }
+
+        [Display(Name = "Sort Field")]
+        public RepoSearchSort? SearchSort { get; set; }
+
+        [Display(Name = "Sort Asc/Dsc")]
+        public SortDirection SortAscDsc { get; set; }
+
+        public List<Uri> Uris;
+
+        public RepoChecker()
+        {
+            Uris = new List<Uri>();
+        }
+
+        public enum RepoSearchSort
+        {
+            Stars = 0,
+            Forks = 1,
+            Updated = 2
+        }
+
+        public enum SortDirection
+        {
+            Ascending = 0,
+            Descending = 1
+        }
+    }
+}
